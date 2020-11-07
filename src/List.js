501a007c6b1d5fb72cc86724685054e9731f37a7
Dennis C <dc5stars@gmail.com>
2020-11-06 19:10:54 -0500

    List.js: refactored to arrow function

--- src/List.js
+++ src/List.js
@@ -3,18 +3,18 @@ import Item from './Item'
 
 
 //nested destructuring format: destructuredObj: {neededProperty}
-function List({stories, search, removeStory, nested: {ownerName}, nested: {pet: {name}}}) {
+List = ({stories, removeStory, nested: {ownerName}, nested: {pet: {name}}}) =>
   //console.log('List spread: ', ...list)
   //console.log("STORIES: ", stories)
-  return (
-    <div>
-      <p>Name: {ownerName}</p>
-      <p>Pet Name: {name}</p>
-      <ul>{stories.map(story => 
-        <Item key={story.objectID} {...story} removeStory={removeStory}/>
-      )}</ul>
+  console.log("B: List") || 
+  <div>
+  <p>Name: {ownerName}</p>
+  <p>Pet Name: {name}</p>
+  <ul>{stories.map(story => 
+    <Item key={story.objectID} {...story} removeStory={removeStory}/>
+  )}</ul>
   </div>
-  )
-}
+
+
 
 export default List
