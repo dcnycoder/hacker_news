f6a6350498cca12c0af12258d8170c7a26a0bbc0
Dennis C <dc5stars@gmail.com>
2020-11-06 19:28:55 -0500

    List.js: React.memo done

--- src/List.js
+++ src/List.js
@@ -3,7 +3,8 @@ import Item from './Item'
 
 
 //nested destructuring format: destructuredObj: {neededProperty}
-const List = ({stories, removeStory, nested: {ownerName}, nested: {pet: {name}}}) =>
+const List = React.memo(
+  ({stories, removeStory, nested: {ownerName}, nested: {pet: {name}}}) =>
   //console.log('List spread: ', ...list)
   //console.log("STORIES: ", stories)
   console.log("B: List") || 
@@ -14,7 +15,6 @@ const List = ({stories, removeStory, nested: {ownerName}, nested: {pet: {name}}}
     <Item key={story.objectID} {...story} removeStory={removeStory}/>
   )}</ul>
   </div>
-
-
+) 
 
 export default List
