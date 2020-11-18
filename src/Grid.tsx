f214cf67bdb3d75b72502b9ba0b29242dbe2a796
dcnycoder <dc5stars@gmail.com>
2020-11-18 17:19:07 -0500

    Grid: before changing vertical alignment

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -6,10 +6,20 @@ const Grid = () => {
 
   // return <div style={{backgroundColor: "lightblue"}}>
   // style={{color: "red"}}
-  return <Container className='w-25'>
-    <Row className="position-relative w-100 align-items-left">
-      <Col className="col-md-3 border">1 of 2</Col>
-      <Col className="col-md-3">2 of 2</Col>
+  return <Container>
+    {/* <Row className="position-relative w-100 align-items-left"></Row> */}
+
+    <Row className="position-relative align-items-left">
+      <Col className='col-md-1'>1 of 3</Col>                                                                                                                                                                        
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col className='col-md-1'>1 of 3</Col>
+      <Col>2 of 3</Col>
+      <Col>3 of 3</Col>
     </Row>
     <Row>
       <Col className="justify-content-start">1 of 3</Col>
