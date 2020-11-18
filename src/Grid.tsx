46098ecefb7bb432b02f4fa06c7dbc9c1627eae7
dcnycoder <dc5stars@gmail.com>
2020-11-18 17:38:39 -0500

    Grid: rows and columns done with simple bootstrap

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -6,38 +6,40 @@ const Grid = () => {
 
   // return <div style={{backgroundColor: "lightblue"}}>
   // style={{color: "red"}}
-  return <Container>
-    {/* <Row className="position-relative w-100 align-items-left"></Row> */}
-
-    <Row className="position-relative align-items-left">
-      <Col className='col-md-1'>1 of 3</Col>                                                                                                                                                                        
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col className='col-md-1'>1 of 3</Col>
-      <Col>2 of 3</Col>
-      <Col>3 of 3</Col>
-    </Row>
-    <Row>
-      <Col className="justify-content-start">1 of 3</Col>
-      <Col>2 of 3</Col>
-      <Col>3 of 3</Col>
-    </Row>
-  </Container>
-  {/* <Container>
-    <Row>
-      <Col>column 1</Col>
-      <Col>
-        column 2
-      </Col>
-      <Col>
-        column 3
-      </Col>
-    </Row>
-  </Container> */}
+  return <div className='container'>
+    <div className='row'>
+      <div className='col'>1</div>
+      <div className='col'>2</div>
+      <div className='col'>3</div>
+    </div>
+    <div className='row'>
+      <div className='col'>1</div>
+      <div className='col'>2</div>
+      <div className='col'>3</div>
+    </div>
+    <div className='row'>
+      <div className='col'>1</div>
+      <div className='col'>2</div>
+      <div className='col'>3</div>
+    </div>
+  </div>
+  // <Container>
+  //   <Row className='align-items-start h-50'>
+  //     <Col>1</Col>
+  //     <Col>2</Col>
+  //     <Col>3</Col>
+  //   </Row>
+  //   <Row className='align-items-center'>
+  //     <Col>1</Col>
+  //     <Col>2</Col>
+  //     <Col>3</Col>
+  //   </Row>
+  //   <Row className='align-items-end'>
+  //     <Col>1</Col>
+  //     <Col>2</Col>
+  //     <Col>3</Col>
+  //   </Row>
+  // </Container>
 }
 
 export default Grid
