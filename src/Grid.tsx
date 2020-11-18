8d1587b22d31edcd25de09dbeffd2fffbe9aee93
dcnycoder <dc5stars@gmail.com>
2020-11-18 18:28:14 -0500

    Grid: col align-self-(start-center-end) works

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,21 +3,19 @@ import React from 'react'
 import {Row, Col, Container} from 'react-bootstrap'
 
 const Grid = () => {
-  return <Container>
+  return <Container fluid>
     <Row className='align-items-start'>
-      <Col className='align-self-start'>1</Col>
-      <Col className='align-self-center'>2</Col>
-      <Col className='align-self-end'>3</Col>
+      <Col className='col-4 align-self-start'>1</Col>
+      <Col className='col-4 align-self-center offset-2'>2</Col>
+      <Col className='col-2 align-self-end'>3</Col>
     </Row>
-    <Row className='align-items-center'>
-      <Col className='align-self-end'>1</Col>
-      <Col>2</Col>
-      <Col>3</Col>
+    <Row className='align-items-center justify-content-center'>
+      <Col className='col-4 align-self-end'>1</Col>
+      <Col className='col-4 offset-2'>2</Col>
     </Row>
-    <Row className='align-items-end'>
-      <Col>1</Col>
-      <Col>2</Col>
-      <Col>3</Col>
+    <Row className='align-items-end justify-content-end'>
+      <Col className='col-4'>1</Col>
+      <Col className='col-4 offset-4'>2</Col>
     </Row>
   </Container>
 }
