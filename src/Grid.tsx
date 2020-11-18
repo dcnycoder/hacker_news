0d141548b17d992358348fcac3685be906f691be
dcnycoder <dc5stars@gmail.com>
2020-11-18 18:08:05 -0500

    Grid: row content vertical alignment works with react bootstrap

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,44 +3,40 @@ import React from 'react'
 import {Row, Col, Container} from 'react-bootstrap'
 
 const Grid = () => {
-
-  // return <div style={{backgroundColor: "lightblue"}}>
-  // style={{color: "red"}}
-  return <div className='container'>
-    <div className='row align-items-start'>
-      <div className='col'>1</div>
-      <div className='col'>2</div>
-      <div className='col'>3</div>
-    </div>
-    <div className='row align-items-center'>
-      <div className='col'>1</div>
-      <div className='col'>2</div>
-      <div className='col'>3</div>
-    </div>
-    <div className='row align-items-end'>
-      <div className='col'>1</div>
-      <div className='col'>2</div>
-      <div className='col'>3</div>
-    </div>
-  </div>
-  
-  // <Container>
-  //   <Row className='align-items-start'>
-  //     <Col>1</Col>
-  //     <Col>2</Col>
-  //     <Col>3</Col>
-  //   </Row>
-  //   <Row className='align-items-center'>
-  //     <Col>1</Col>
-  //     <Col>2</Col>
-  //     <Col>3</Col>
-  //   </Row>
-  //   <Row className='align-items-end'>
-  //     <Col>1</Col>
-  //     <Col>2</Col>
-  //     <Col>3</Col>
-  //   </Row>
-  // </Container>
+  return <Container>
+    <Row className='align-items-start'>
+      <Col>1</Col>
+      <Col>2</Col>
+      <Col>3</Col>
+    </Row>
+    <Row className='align-items-center'>
+      <Col>1</Col>
+      <Col>2</Col>
+      <Col>3</Col>
+    </Row>
+    <Row className='align-items-end'>
+      <Col>1</Col>
+      <Col>2</Col>
+      <Col>3</Col>
+    </Row>
+  </Container>
+    // return <div className='container'>
+  //   <div className='row align-items-start'>
+  //     <div className='col'>1</div>
+  //     <div className='col'>2</div>
+  //     <div className='col'>3</div>
+  //   </div>
+  //   <div className='row align-items-center'>
+  //     <div className='col'>1</div>
+  //     <div className='col'>2</div>
+  //     <div className='col'>3</div>
+  //   </div>
+  //   <div className='row align-items-end'>
+  //     <div className='col'>1</div>
+  //     <div className='col'>2</div>
+  //     <div className='col'>3</div>
+  //   </div>
+  // </div>
 }
 
 export default Grid
