5a5d8a58d74260971c2f08b0bbc8633a708ca604
dcnycoder <dc5stars@gmail.com>
2021-03-12 20:34:51 -0500

    AppNew: list: content vertically aligned to the top

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
       <AppNew />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('container')
 );
 
 // If you want your app to work offline and load faster, you can change
