89766ce2a90db971103e7d9219372a50edf8e76d
dcnycoder <dc5stars@gmail.com>
2021-03-16 20:51:40 -0400

    Updates

---
+++ precache-manifest.ee564d113d43bbe40af647237150ab6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c24bbaff13cb316e2ba3dd889862829",
+    "url": "/hacker_news/index.html"
+  },
+  {
+    "revision": "a5e27b7b3b073b7db4f9",
+    "url": "/hacker_news/static/css/main.45268cad.chunk.css"
+  },
+  {
+    "revision": "a71315ef6d406fd96627",
+    "url": "/hacker_news/static/js/2.78ede939.chunk.js"
+  },
+  {
+    "revision": "a05f24f8d8a8386264ef2d05e88aa37b",
+    "url": "/hacker_news/static/js/2.78ede939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e27b7b3b073b7db4f9",
+    "url": "/hacker_news/static/js/main.fab5c2d1.chunk.js"
+  },
+  {
+    "revision": "0fe74fc9920357125385",
+    "url": "/hacker_news/static/js/runtime-main.94a62fe7.js"
+  }
+]);
